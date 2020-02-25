a6b01171c50c8cd48fe3a0c1a26f59ac6fefd6ca
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2020-02-24 20:17:23 -0500

    Cache Middleware ADD: Se agrega middleware para manejar cache. Aun por configurar

---
+++ app/src/middlewares/cacheMiddleware.js
@@ -0,0 +1,21 @@
+const mcache = require('memory-cache');
+const container = require('../startup/container');
+const config = container.resolve('Config');
+
+module.exports = (req, res, next) => {
+  return (req,res,next) => {
+    const key = config.CACHE_KEY + req.originUrl || req.url;
+    const cachedBody = mcache.get(key);
+    
+    if(cachedBody){
+      return res.send(JSON.parse(cachedBody));
+    }else{
+      res.sendResponse = res.send;
+      res.send = body => {
+        mcache.put(key, sendResponse, duration*1000);
+        res.sendResponse(body);
+      };
+      next();
+    }
+  }
+}
